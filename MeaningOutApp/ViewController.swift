8a08cc5ddedd895debb2d4fbd562314ba705e2ac
knockknock <yangban11111@gmail.com>
2024-06-13 16:28:01 +0900

    Initial Commit

---
+++ MeaningOutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MeaningOutApp
+//
+//  Created by 양승혜 on 6/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
