0caeb336bebf420fdcb19d58363b02f2eada7b5c
knockknock <yangban11111@gmail.com>
2024-06-13 17:09:24 +0900

    splash 화면 2초간 유지, 스플래시 화면 ui 위치 설정, constant값들 열거, hexCode extension  적용

---
+++ MeaningOutApp/Constant/Constant.swift
@@ -0,0 +1,28 @@
+//
+//  Constant.swift
+//  MeaningOutApp
+//
+//  Created by 양승혜 on 6/13/24.
+//
+
+import UIKit
+
+enum Constant {
+    enum FontColors {
+        static let mainOrange = UIColor(hexCode: "ef8947")
+        static let black = UIColor(hexCode: "000000")
+        static let gray = UIColor(hexCode: "828282")
+        static let darkGray = UIColor(hexCode: "4c4c4c")
+        static let lightGray = UIColor(hexCode: "cdcdcd")
+        static let white = UIColor(hexCode: "ffffff")
+    }
+    
+    enum buttonImages {
+        static let serachButton = UIImage(systemName: "magnifyingglass")
+        static let personButton = UIImage(systemName: "person")
+        static let nextButton = UIImage(systemName: "chevron.right")
+        static let clockButton = UIImage(systemName: "clock")
+        static let closeButton = UIImage(systemName: "xmark")
+        static let cameraButton = UIImage(systemName: "camera.fill")
+    }
+}
